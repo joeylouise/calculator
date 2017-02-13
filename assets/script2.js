f0bbd18339070fe70025fcf5e91d8dabcda625f3
Joey Scott <joannascott29@hotmail.co.uk>
2017-02-13 21:04:37 +0000

    Add eval() function

--- assets/script2.js
+++ assets/script2.js
@@ -3,11 +3,19 @@ var keys = document.querySelectorAll('button');
 for (var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(){
 		var screen = document.getElementById('screen');
+		var screenValue = screen.innerHTML;
 		var btnValue = this.innerHTML;
 
 		if (btnValue === 'C') {
 			screen.innerHTML = '';
-		} else {
+		}
+
+		else if (btnValue === '=') {
+			screenValue = screenValue.replace(/x/, '*').replace(/÷/, '/');
+			screen.innerHTML = eval(screenValue);
+		}
+
+		else {
 			screen.innerHTML += btnValue;
 		}
 	}
