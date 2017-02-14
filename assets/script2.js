396682c8f6d48ef65d76bc9a5799401e7f90f6a6
Joey Scott <joannascott29@hotmail.co.uk>
2017-02-14 13:31:37 +0000

    Add functionality to prevent more than one operator being added in a row

--- assets/script2.js
+++ assets/script2.js
@@ -1,22 +1,38 @@
 var keys = document.querySelectorAll('button');
+var operators = ['+','x','÷','-'];
 
 for (var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(){
-		var screen = document.getElementById('screen');
-		var screenValue = screen.innerHTML;
+		var display = document.getElementById('display');
+		var displayValue = display.innerHTML;
 		var btnValue = this.innerHTML;
 
+		//If C button is presssed, clear the screen
 		if (btnValue === 'C') {
-			screen.innerHTML = '';
+			display.innerHTML = '';
 		}
 
+		//Replace 'x' and '÷' with mathematical operators, respectively '*' and '/'
 		else if (btnValue === '=') {
-			screenValue = screenValue.replace(/x/, '*').replace(/÷/, '/');
-			screen.innerHTML = eval(screenValue);
+			displayValue = displayValue.replace(/x/g, '*').replace(/÷/g, '/');
+			//Evaluate the string contained in the display
+			console.log(displayValue);
+			display.innerHTML = eval(displayValue);
 		}
 
+		//If an operator is pressed
+		else if (operators.indexOf(btnValue) !== -1) {
+			//Set variable for last character in the display
+			var lastChar = displayValue[displayValue.length -1];
+			//If the last character in the display is not an operator, add btnValue to display
+			if (operators.indexOf(lastChar) === -1) {
+				display.innerHTML += btnValue;
+			}
+		}
+
+		//If other button is pressed, add button's innerHTML to the screen
 		else {
-			screen.innerHTML += btnValue;
+			display.innerHTML += btnValue;
 		}
 	}
 }
